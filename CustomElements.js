99929c818024140a89dcc04b95ef229d211dce2a
defnoteid <157801052+defnoteid@users.noreply.github.com>
2024-01-26 21:53:13 +0800

    Update CustomElements.js

--- CustomElements.js
+++ CustomElements.js
@@ -3,14 +3,7 @@ SpaceTode`
         prop state SOLID
         colour "#006eff"
         category "Custom"
-        maybe(1/2) {
-            given A (element) => element.state > SOLID && element.state !== EFFECT && element != bluonium
-            
-            for(xz) {
-                @A => .@
-            }
-        }
 
-        mimic(Powder)
+        mimic(Sand)
     }
 `
